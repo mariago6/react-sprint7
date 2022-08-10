8aef2aaca412571fd06317a90fa99f97634ea9f1
Maria Go <merigon_93@hotmail.com>
2022-08-10 16:59:24 +0100

    exercise_1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,62 @@
 import React from 'react'
+import { useState } from 'react';
+import { formData } from './formData';
+
+
+const getFormattedPrice = (price) => `${price.toFixed(0)}€`;
 
 function App() {
+  const [checkedState, setCheckedState] = useState(
+    new Array(formData.length).fill(false)
+  );
+
+  const [total, setTotal] = useState(0);
+
+  const handleOnChange = (position) => {
+    const updatedCheckedState = checkedState.map((item, index) =>
+      index === position ? !item : item
+    );
+
+    setCheckedState(updatedCheckedState);
+
+    const totalPrice = updatedCheckedState.reduce(
+      (sum, currentState, index) => {
+        if (currentState === true) {
+          return sum + formData[index].price;
+        }
+        return sum;
+      },
+      0
+    );
+
+    setTotal(totalPrice);
+  };
+
   return (
-    <div className="App">
-      
+    <div>
+      <p>What do you want to do?</p>
+      <p>
+        {formData.map(({name}, index) => {
+          return (
+            <div>
+              <input
+                type="checkbox"
+                id={index}
+                name={name}
+                value={name}
+                checked={checkedState[index]}
+                onChange={() => handleOnChange(index)}
+              />
+              <label htmlFor={index}>{name}</label>
+              <br />
+              <br/>
+            </div> 
+          );
+        })}
+      </p>
+      <div>
+        <p>Price: {getFormattedPrice(total)}</p>
+      </div>
     </div>
   );
 }
---
+++ src/formData.js
@@ -0,0 +1,15 @@
+export const formData = [
+  {
+    name: 'A website (500€)',
+    price: 500
+  },
+  {
+    name: 'A SEO consultancy (300€)',
+    price: 300
+  },
+  {
+    name: 'A Google Ads campaign (200€)',
+    price: 200
+  },
+]; 
+
